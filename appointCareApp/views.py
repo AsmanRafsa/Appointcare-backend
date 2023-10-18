53fdbce5f36aae1772683192d64c9c8c78c31eb2
JoyceMideva <mideva061@gmail.com>
2023-10-18 14:33:06 +0200

    worked on the views part

--- appointCareApp/views.py
+++ appointCareApp/views.py
@@ -4,7 +4,7 @@ from rest_framework import status
 from rest_framework.views import APIView 
 from django.http import Http404
 from rest_framework.response import Response
-from serializers import HospitalSerializer
+from .serializers import HospitalSerializer
 
 # Create your views here.
 class HospitalView(APIView):
